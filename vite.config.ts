d2902e22e817129b90a8e4e02277ea7b5537afce
brosing <singgihnn@gmail.com>
2024-06-24 15:43:19 +0700

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			includeAssets: ['logo.jpg', 'apple-touch-icon.png', 'images/ball.png'],
+			manifest: {
+        name: 'Poke Match',
+        short_name: 'PokeMatch',
+        description: 'Memory Match Game with Pokemons',
+        theme_color: '#ffffff',
+        icons: []
+      }
+		})
+	]
 });
