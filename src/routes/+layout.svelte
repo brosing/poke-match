1bba39d4f5cf0249be6bc6f13a2f51db6473a7e9
brosing <singgihnn@gmail.com>
2024-08-07 12:55:34 +0700

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-  import "../app.css";
+	import '../app.css';
 
-  import { setColorSchemeContext } from '$lib/contexts/color-scheme';
-  import { setLeaderboardContext } from '$lib/contexts/leaderboard';
-  /** @type {import('./$types).LayoutData} */
-  export let data;
-  setColorSchemeContext(data.colorScheme);
-  setLeaderboardContext([]);
+	import { setColorSchemeContext } from '$lib/contexts/color-scheme';
+	import { setLeaderboardContext } from '$lib/contexts/leaderboard';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
+
+	/** @type {import('./$types).LayoutData} */
+	export let data;
+	setColorSchemeContext(data.colorScheme);
+	setLeaderboardContext([]);
 </script>
 
 <slot />
